fca378b142353a9334e62d2e810ddb63f72e8fcd
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2025-07-07 07:25:25 -0500

    Se agrego la clase cliente.cs en la carpeta models, se implemento las variables necesarias.

---
+++ ExamenN3AyalaProgramacion/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ExamenN3AyalaProgramacion.Models
+{
+    [Table("clientes")]
+    public class Cliente
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Nombre { get; set; }
+
+        [MaxLength(250)]
+        public string Empresa { get; set; }
+
+        public int AntiguedadMeses { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
