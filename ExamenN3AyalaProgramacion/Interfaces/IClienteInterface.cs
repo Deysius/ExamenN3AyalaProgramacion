93f92914edbc3dbe8ce96e37b92a6f464d2f7d03
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2025-07-07 07:39:26 -0500

    Se agrego la interfaz, para luego implementarla en el repositorio.

---
+++ ExamenN3AyalaProgramacion/Interfaces/IClienteInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamenN3AyalaProgramacion.Models;
+using SQLite;
+
+namespace ExamenN3AyalaProgramacion.Interfaces
+{
+    public interface IClienteInterface
+    {
+        public void Init();
+        public Task InicializarBaseDeDatosAsync();
+        public Task AgregarClienteAsync(ExamenN3AyalaProgramacion.Models.Cliente cliente);
+        public Task<List<ExamenN3AyalaProgramacion.Models.Cliente>> ObtenerClientesAsync();
+        public bool EsAntiguedadValida(int antiguedadMeses);
+        public Task AgregarLogAsync(string nombreCliente);
+        public Task<List<string>> ObtenerLogsAsync();
+        public Task EliminarTodosLosClientesAsync();
+        public Task EliminarLogsAsync();
+    }
+}
+
